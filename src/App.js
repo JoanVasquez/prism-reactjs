3f2e43cec1b21ed4e7eddd9a0ce3be86c209d532
Joan Vasquez <yoloprogramo22@gmail.com>
2018-11-13 10:06:48 -0400

    Highlighting code in JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PrismCode from "react-prism";
 
 class App extends Component {
+  
   render() {
+    let java = 'String name = "Joan Vasquez";\nSystem.out.println(name);';
+    let javascript = 'let name = "Joan Vasquez;\nconsole.log(name)";';
+    let html = '<div>\n    <p>\n      Joan Vasquez\n    </p>\n</div>';
+    let css = '.container {\n   background-color: "#000";\n}';
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h2>Java</h2>
+        <pre>
+          <PrismCode className="language-java">
+            {java}
+          </PrismCode>
+        </pre>
+
+        <h2>JavaScript</h2>
+        <pre>
+          <PrismCode className="language-javascript">
+            {javascript}
+          </PrismCode>
+        </pre>
+
+        <h2>Html</h2>
+        <pre>
+          <PrismCode className="language-html">
+            {html}
+          </PrismCode>
+        </pre>
+
+        <h2>Css</h2>
+        <pre>
+          <PrismCode className="language-css">
+            {css}
+          </PrismCode>
+        </pre>
+
       </div>
     );
   }
